9f54815dafc39097036a557bcc58ff2419400c94
Chinavath Ganapathi <ganapathichinavath@gmail.com>
2026-06-09 01:00:20 +0530

    fixed backend issue

--- Frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import useStore from '../store/useStore'
-export default function ProtectedRoute() {
-  const token = useStore((s) => s.token)
-  return token ? <Outlet /> : <Navigate to="/login" replace />
-}
